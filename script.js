ec68e234798d2a6fac69c6354aca81c22c8e1f26
ramshakhan123 <49521382+ramshakhan123@users.noreply.github.com>
2019-07-23 23:29:57 +0500

    added files...

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ let numberGame = document.querySelector(".number-game");
 let easyGameArea = document.querySelector("#easy-game-area");
 let mediumGameArea = document.querySelector("#medium-game-area");
 let hardGameArea = document.querySelector("#hard-game-area");
-
+let goodJobBox = document.querySelector("#box");
+let tooSmallBox = document.querySelector("#box2");
+let tooBigBox = document.querySelector("#box3")
 let numberForEach = 0;
 let computerNumber;
-
 
 function easy() {
     numberGame.style.display = "none";
     easyGameArea.style.display = "block";
+    document.querySelector("body").style.backgroundImage = "none";
     numberForEach = 51;
     computerNumber = parseInt(Math.random() * numberForEach);
 }
@@ -17,9 +19,29 @@ function easy() {
 function checkCorrectNumber() {
     let userInput = parseInt(document.querySelector("#userGuessNumber").value);
     if (userInput > computerNumber) {
-        alert("Too big");
-    }else if (userInput < computerNumber) {
-        alert("too small");
-    }else 
-    alert("congratulations");
+        tooBigBox.style.display = "block";
+        easyGameArea.style.display = "none";
+    } else if (userInput < computerNumber) {
+        tooSmallBox.style.display = "block";
+        easyGameArea.style.display = "none";
+    } else {
+        goodJobBox.style.display = "block";
+        easyGameArea.style.display = "none";
+    }
 }
+
+function close2() {
+    tooSmallBox.style.display = "none";
+    easyGameArea.style.display = "block";
+
+}
+function close3() {
+    tooBigBox.style.display = "none";
+    easyGameArea.style.display = "block";
+
+}
+function close() {
+    goodJobBox.style.display = "none";
+    easyGameArea.style.display = "block";
+}
+
