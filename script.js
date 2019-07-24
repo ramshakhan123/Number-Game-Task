3302a25f6c0944b8a1a7bed79e4b4d9e64f103ce
ramshakhan123 <49521382+ramshakhan123@users.noreply.github.com>
2019-07-24 12:41:40 +0500

    added files...

--- script.js
+++ script.js
@@ -11,11 +11,12 @@ let computerNumber;
 function easy() {
     numberGame.style.display = "none";
     easyGameArea.style.display = "block";
+    hardGameArea.style.display = "none";
+    mediumGameArea.style.display = "none";
     document.querySelector("body").style.backgroundImage = "none";
     numberForEach = 51;
     computerNumber = parseInt(Math.random() * numberForEach);
 }
-
 function checkCorrectNumber() {
     let userInput = parseInt(document.querySelector("#userGuessNumber").value);
     if (userInput > computerNumber) {
@@ -29,7 +30,6 @@ function checkCorrectNumber() {
         easyGameArea.style.display = "none";
     }
 }
-
 function close2() {
     tooSmallBox.style.display = "none";
     easyGameArea.style.display = "block";
@@ -44,4 +44,81 @@ function close() {
     goodJobBox.style.display = "none";
     easyGameArea.style.display = "block";
 }
+
+function medium() {
+    numberGame.style.display = "none";
+    mediumGameArea.style.display = "block";
+    easyGameArea.style.display = "none";
+    hardGameArea.style.display = "none";
+    document.querySelector("body").style.backgroundImage = "none";
+    numberForEach = 100;
+    computerNumber = parseInt(Math.random() * numberForEach);
+}
+function checkCorrectNumber2() {
+    let userInputMedium = parseInt(document.querySelector("#userGuessNumber2").value);
+    if (userInputMedium > computerNumber) {
+        tooBigBox.style.display = "block";
+        mediumGameArea.style.display = "none";
+    } else if (userInputMedium < computerNumber) {
+        tooSmallBox.style.display = "block";
+        mediumGameArea.style.display = "none";
+    } else {
+        goodJobBox.style.display = "block";
+        mediumGameArea.style.display = "none";
+    }
+}
+function close2() {
+    tooSmallBox.style.display = "none";
+    mediumGameArea.style.display = "block";
+
+}
+function close3() {
+    tooBigBox.style.display = "none";
+    mediumGameArea.style.display = "block";
+
+}
+function close() {
+    goodJobBox.style.display = "none";
+    mediumGameArea.style.display = "block";
+}
+
+function hard() {
+    numberGame.style.display = "none";
+    hardGameArea.style.display = "block";
+    mediumGameArea.style.display = "none";
+    easyGameArea.style.display = "none";
+    document.querySelector("body").style.backgroundImage = "none";
+    numberForEach = 300;
+    computerNumber = parseInt(Math.random() * numberForEach);
+}
+function checkCorrectNumber3() {
+    let userInputHard = parseInt(document.querySelector("#userGuessNumber3").value);
+    if (userInputHard > computerNumber) {
+        tooBigBox.style.display = "block";
+        hardGameArea.style.display = "none";
+    } else if (userInputHard < computerNumber) {
+        tooSmallBox.style.display = "block";
+        hardGameArea.style.display = "none";
+    } else {
+        goodJobBox.style.display = "block";
+        hardGameArea.style.display = "none";
+    }
+}
+function close2() {
+    tooSmallBox.style.display = "none";
+    hardGameArea.style.display = "block";
+
+}
+function close3() {
+    tooBigBox.style.display = "none";
+    hardGameArea.style.display = "block";
+
+}
+function close() {
+    goodJobBox.style.display = "none";
+    hardGameArea.style.display = "block";
+}
+
+
+
 
